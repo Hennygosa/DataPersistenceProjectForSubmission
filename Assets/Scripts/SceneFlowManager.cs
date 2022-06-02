2f8d244d8e677233d198648a768e00b70615dc1e
Dmitry.Kopylov <diemon_1992@mail.ru>
2022-06-02 18:32:11 +0500

    Scene Flow
    Start, Exit, Back To Menu buttons functionality

---
+++ Assets/Scripts/SceneFlowManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SceneFlowManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
